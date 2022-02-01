fcad1171ccc14fdaa12db40c201048f11788279e
DavidTret <davidtrettin@gmail.com>
2022-02-01 14:25:59 -0600

    Added comments to OpenGL code and created a Texture class for handling OpenGL texture operations.

--- unoLib/src/GameLib.cpp
+++ unoLib/src/GameLib.cpp
@@ -8,6 +8,7 @@
 #include "GLFW/glfw3.h"
 #include "stb/stb_image.h"
 
+#include "uno/Texture.h"
 #include "uno/shaderClass.h"
 #include "uno/VAO.h"
 #include "uno/VBO.h"
@@ -227,9 +228,10 @@ OpenGLTest()
     //
     // Refer to this video for an OpenGL tutorial.
     // https://www.youtube.com/watch?v=45MIykWJ-C4&ab_channel=freeCodeCamp.org
-    
+
     // Initialize GLFW
     glfwInit();
+
     // Tell GLFW what version of OpenGL we are using.
     // In this case we are using OpenGL 3.3.
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -275,87 +277,83 @@ OpenGLTest()
     }
     // Introduce the window into the current context.
     glfwMakeContextCurrent(window);
+
     // Load GLAD so it configures OpenGL.
     gladLoadGL();
     // Specify the viewport of OpenGL in the Window.
+    // In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800.
     glViewport(0, 0, 800, 800);
-    
 
+
+    // Generates Shader object using shaders default.vert and default.frag.
     Shader shaderProgram("../../../../unoLib/rsrc/default.vert", "../../../../unoLib/rsrc/default.frag");
 
+
+
+    // Generates Vertex Array Object and binds it.
     VAO VAO1;
     VAO1.Bind();
 
+    // Generates Vertex Buffer Object and links it to vertices.
     VBO VBO1(vertices, sizeof(vertices));
+    // Generates Element Buffer Object and links it to indices.
     EBO EBO1(indices, sizeof(indices));
-    // Links VBO to VAO
+
+    // Links VBO attributes such as coordinates and colors to VAO.
     VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
     VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    // Unbind all to prevent accidentally modifying them.
     VAO1.Unbind();
     VBO1.Unbind();
     EBO1.Unbind();
 
+    // Gets ID of uniform called "scale"
     GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
+
+
+
     // Texture
-    int widthImg, heightImg, numColCh;
-    stbi_set_flip_vertically_on_load(true);
-    unsigned char* bytes = stbi_load("../../../../unoLib/rsrc/pop_cat.jpg", &widthImg, &heightImg, &numColCh, 0);
+    Texture popCat("../../../../unoLib/rsrc/pop_cat.jpg", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
+    popCat.texUnit(shaderProgram, "tex0", 0);
 
-    GLuint texture;
-    glGenTextures(1, &texture);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texture);
+   
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthImg, heightImg, 0, GL_RGB, GL_UNSIGNED_BYTE, bytes);
-    glGenerateMipmap(GL_TEXTURE_2D);
-
-    stbi_image_free(bytes);
-    glBindTexture(GL_TEXTURE_2D, 0);
-
-    GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
-    shaderProgram.Activate();
-    glUniform1i(tex0Uni, 0);
-
-    // Specify the color of the background.
-    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-    // Clear the back buffer and assign the new color to it.
-    glClear(GL_COLOR_BUFFER_BIT);
-    // Swap the back buffer with the front buffer.
-    glfwSwapBuffers(window);
-    // Main while loop.
+    // Main while loop
     while (!glfwWindowShouldClose(window)) {
-
+        // Specify the color of the background.
         glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        // Clear the back buffer and assign the new color to it.
         glClear(GL_COLOR_BUFFER_BIT);
         // Tell OpenGL which Shader Program we want to use.
         shaderProgram.Activate();
+        // Assigns a value to the uniform; NOTE: Must always be done after activating the Shader Program.
         glUniform1f(uniID, 0.5f);
-        glBindTexture(GL_TEXTURE_2D, texture);
-        // Bind the VAO so OpenGL knows how to use it.
+        // Binds texture so that it appears in rendering.
+        popCat.Bind();
+        // Bind the VAO so OpenGL knows to use it.
         VAO1.Bind();
-        // Draw the triangle using the GL_TRIANGLES primitive.
+        // Draw primitives, number of indices, datatype of indices, index of indices.
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // Swap the back buffer with the front buffer.
         glfwSwapBuffers(window);
         // Take care of all GLFW events.
         glfwPollEvents();
     }
 
+
+
     // Delete all the objects we have created.
     VAO1.Delete();
     VBO1.Delete();
     EBO1.Delete();
-    glDeleteTextures(1, &texture);
+    popCat.Delete();
     shaderProgram.Delete();
-
-    // Clean up.
+    // Delete window before ending the program.
     glfwDestroyWindow(window);
+    // Terminate GLFW before ending the program.
     glfwTerminate();
 }
--- unoLib/src/VAO.cpp
+++ unoLib/src/VAO.cpp
@@ -1,11 +1,13 @@
 #include "uno/VAO.h"
 
+// Constructor that generates a VAO ID
 VAO::VAO()
 {
 	glGenVertexArrays(1, &ID);
 }
 
-void VAO::LinkAttrib(VBO VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset)
+// Links a VBO Attribute such as a position or color to the VAO
+void VAO::LinkAttrib(VBO& VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset)
 {
 	VBO.Bind();
 	glVertexAttribPointer(layout, numComponents, type, GL_FALSE, stride, offset);
@@ -13,16 +15,19 @@ void VAO::LinkAttrib(VBO VBO, GLuint layout, GLuint numComponents, GLenum type, 
 	VBO.Unbind();
 }
 
+// Binds the VAO
 void VAO::Bind()
 {
 	glBindVertexArray(ID);
 }
 
+// Unbinds the VAO
 void VAO::Unbind()
 {
 	glBindVertexArray(0);
 }
 
+// Deletes the VAO
 void VAO::Delete()
 {
 	glDeleteVertexArrays(1, &ID);
