54226c82468e7ad648f7d2f9bd0ba1ff4180adcd
DavidTret <davidtrettin@gmail.com>
2022-02-01 13:59:51 -0600

    Added support for OpenGL textures in the shader programs.

--- unoLib/src/GameLib.cpp
+++ unoLib/src/GameLib.cpp
@@ -6,6 +6,8 @@
 
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include "stb/stb_image.h"
+
 #include "uno/shaderClass.h"
 #include "uno/VAO.h"
 #include "uno/VBO.h"
@@ -236,7 +238,7 @@ OpenGLTest()
     // so that means we only have the modern functions.
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     // Vertices coordinates
-    GLfloat vertices[] = {
+    GLfloat verticesOld[] = {
         //                  Coordinats                           Colors
         -0.5f,      -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f, 0.3f,  0.02f,  // Lower left corner
         0.5f,       -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f, 0.3f,  0.02f,  // Lower right corner
@@ -245,11 +247,22 @@ OpenGLTest()
         0.5f / 2,   0.5f * float(sqrt(3)) / 6,      0.0f,   0.9f, 0.45f, 0.17f,  // Inner right corner
         0.0f,       -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f, 0.3f,  0.02f   // Inner lower corner
     };
+    GLfloat vertices[] = {
+        // Coordinats            Colors
+        -0.5f, -0.5f, 0.0f,     1.0f, 0.0f,  0.0f,  0.0f, 0.0f, // Lower left corner
+        -0.5f, 0.5f,  0.0f,     0.0f, 1.0f,  0.0f,  0.0f, 1.0f, // Upper left corner
+        0.5f,  0.5f,  0.0f,     0.0f, 0.0f,  1.0f,  1.0f, 1.0f, // Upper right corner
+        0.5f,  -0.5f, 0.0f,     1.0f, 1.0f,  1.0f,  1.0f, 0.0f  // Lower right corner
+    };
     // Indices array
-    GLuint indices[] = {
+    GLuint indicesOld[] = {
         0, 3, 5, // Lower left triangle
         3, 2, 4, // Lower right triangle
         5, 4, 1 // Upper triangle
+    };
+    GLuint indices[] = {
+        0, 2, 1, // Upper triangle
+        0, 3, 2  // Lower triangle
     };
 
     // Create a GLFWwindow object of 800 by 800 pixels, naming it "UNO"
@@ -276,13 +289,40 @@ OpenGLTest()
     VBO VBO1(vertices, sizeof(vertices));
     EBO EBO1(indices, sizeof(indices));
     // Links VBO to VAO
-    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     VAO1.Unbind();
     VBO1.Unbind();
     EBO1.Unbind();
 
     GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+    // Texture
+    int widthImg, heightImg, numColCh;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* bytes = stbi_load("../../../../unoLib/rsrc/pop_cat.jpg", &widthImg, &heightImg, &numColCh, 0);
+
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthImg, heightImg, 0, GL_RGB, GL_UNSIGNED_BYTE, bytes);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(bytes);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+    shaderProgram.Activate();
+    glUniform1i(tex0Uni, 0);
 
     // Specify the color of the background.
     glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
@@ -298,10 +338,11 @@ OpenGLTest()
         // Tell OpenGL which Shader Program we want to use.
         shaderProgram.Activate();
         glUniform1f(uniID, 0.5f);
+        glBindTexture(GL_TEXTURE_2D, texture);
         // Bind the VAO so OpenGL knows how to use it.
         VAO1.Bind();
         // Draw the triangle using the GL_TRIANGLES primitive.
-        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
         // Take care of all GLFW events.
         glfwPollEvents();
@@ -311,6 +352,7 @@ OpenGLTest()
     VAO1.Delete();
     VBO1.Delete();
     EBO1.Delete();
+    glDeleteTextures(1, &texture);
     shaderProgram.Delete();
 
     // Clean up.
