e58ced9fec98895f7562feb06d60011ce772f4a2
DavidTret <davidtrettin@gmail.com>
2022-02-02 09:24:36 -0600

    Added a camera class for interaction with the window.

--- unoLib/src/GameLib.cpp
+++ unoLib/src/GameLib.cpp
@@ -16,6 +16,7 @@
 #include "uno/VAO.h"
 #include "uno/VBO.h"
 #include "uno/EBO.h"
+#include "uno/Camera.h"
 
 
 //
@@ -229,16 +230,18 @@ ColorString(bool bSelected, Card::Color eColor, std::string& Text)
 const unsigned int width = 800;
 const unsigned int height = 800;
 // Vertices coordinates
-GLfloat vertices[] = {
-    // Coordinats        /    Colors          /   TexCoord
-    -0.5f, 0.0f,  0.5f,     0.83f, 0.70f,  0.44f,  0.0f, 0.0f,
-    -0.5f, 0.0f, -0.5f,     0.83f, 0.70f,  0.44f,  5.0f, 0.0f,
-     0.5f, 0.0f, -0.5f,     0.83f, 0.70f,  0.44f,  0.0f, 0.0f,
-     0.5f, 0.0f,  0.5f,     0.83f, 0.70f,  0.44f,  5.0f, 0.0f,
-     0.0f, 0.8f,  0.0f,     0.92f, 0.86f,  0.76f,  2.5f, 5.0f
+GLfloat vertices[] =
+{ //     COORDINATES     /        COLORS      /   TexCoord  //
+    -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+    -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+     0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+     0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+     0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
 };
+
 // Indices for vertices order
-GLuint indices[] = {
+GLuint indices[] =
+{
     0, 1, 2,
     0, 2, 3,
     0, 1, 4,
@@ -288,7 +291,6 @@ OpenGLTest()
     Shader shaderProgram("../../../../unoLib/rsrc/default.vert", "../../../../unoLib/rsrc/default.frag");
 
 
-
     // Generates Vertex Array Object and binds it.
     VAO VAO1;
     VAO1.Bind();
@@ -307,10 +309,6 @@ OpenGLTest()
     VBO1.Unbind();
     EBO1.Unbind();
 
-    // Gets ID of uniform called "scale"
-    GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
-
-
 
 
     // Texture
@@ -319,12 +317,11 @@ OpenGLTest()
 
 
 
-    // Variables that help the rotation of the pyramid
-    float rotation = 0.0f;
-    double prevTime = glfwGetTime();
-
     // Enables the Depth Buffer
     glEnable(GL_DEPTH_TEST);
+
+    // Creates camera object
+    Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
 
     // Main while loop
     while (!glfwWindowShouldClose(window)) {
@@ -335,33 +332,11 @@ OpenGLTest()
         // Tell OpenGL which Shader Program we want to use.
         shaderProgram.Activate();
 
-        // Simple timer
-        double crntTime = glfwGetTime();
-        if (crntTime - prevTime >= 1 / 60) {
-            rotation += 0.5f;
-            prevTime = crntTime;
-        }
+        // Handles camera inputs
+        camera.Inputs(window);
+        // Updates and exports the camera matrix to the Vertex Shader
+        camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
 
-        // Initializes matrices so they are not the null matrix
-        glm::mat4 model = glm::mat4(1.0f);
-        glm::mat4 view = glm::mat4(1.0f);
-        glm::mat4 proj = glm::mat4(1.0f);
-
-        // Assigns different transformations to each matrix
-        model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
-        view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
-        proj = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
-
-        // Outputs the matrices into the Vertex Shader
-        int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-        int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
-        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
-
-        // Assigns a value to the uniform; NOTE: Must always be done after activating the Shader Program
-        glUniform1f(uniID, 0.5f);
         // Binds texture so that it appears in rendering.
         brickTex.Bind();
         // Bind the VAO so OpenGL knows to use it.
