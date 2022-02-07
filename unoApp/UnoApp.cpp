ac0cae9b91d9355d41b13e783fe6ca0d06341a3a
DavidTret <davidtrettin@gmail.com>
2022-02-07 12:52:25 -0600

    Updated the deck image to make spaces between cards transparent, and displayed random cards to the window.

--- unoApp/UnoApp.cpp
+++ unoApp/UnoApp.cpp
@@ -14,6 +14,9 @@ public:
 
 	olc::Sprite* sprDemo = nullptr;
 	olc::Decal* decDemo = nullptr;
+	size_t nRow = 0;
+	size_t nCol = 0;
+	float timer = 0.0;
 
 public:
 	bool OnUserCreate() override
@@ -28,7 +31,23 @@ public:
 	{
 		Clear(olc::VERY_DARK_BLUE);
 		olc::vf2d mouse = { float(GetMouseX()), float(GetMouseY())};
-		DrawDecal(mouse, decDemo);
+
+		timer += fElapsedTime;
+		//if (false){
+		if (timer > 0.5) {
+			timer = 0.0;
+			nRow = std::rand() % 6;
+			size_t nRowLen = nRow == 5 ? 6 : 10;
+			nCol = std::rand() % nRowLen;
+		}
+
+		olc::vf2d dimCard = { 188, 282 };
+		olc::vf2d offsetCard = { 20, 18 };
+
+		offsetCard.x += nCol * dimCard.x;
+		offsetCard.y += nRow * dimCard.y;
+
+		DrawPartialDecal(mouse, decDemo, offsetCard, dimCard);
 
 		return true;
 	}
@@ -37,7 +56,8 @@ public:
 int main()
 {
 	Example demo;
-	if (demo.Construct(256, 240, 4, 4))
+	if (demo.Construct(1280, 720, 1, 1))
+	//if (demo.Construct(320, 180, 4, 4))
 		demo.Start();
 	return 0;
 }
