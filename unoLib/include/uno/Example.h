2857edd995f5e1dfb4ce08ef2f924c002940c8cd
DavidTret <davidtrettin@gmail.com>
2022-02-07 16:19:29 -0600

    Added rotation to the card display.

--- unoLib/include/uno/Example.h
+++ unoLib/include/uno/Example.h
@@ -7,12 +7,14 @@ public:
 	Example();
 	bool OnUserCreate() override;
 	bool OnUserUpdate(float fElapsedTime) override;
+	olc::vf2d GetCardOffset(int nRow, int nCol);
 
 public:
 
 	olc::Sprite* sprDemo = nullptr;
 	olc::Decal* decDemo = nullptr;
-	size_t nRow = 0;
-	size_t nCol = 0;
+	olc::vf2d dimCard = { 94, 141 };
+	int nRow = 0;
+	int nCol = 0;
 	float timer = 0.0;
 };
--- unoLib/src/Example.cpp
+++ unoLib/src/Example.cpp
@@ -21,20 +21,26 @@ bool Example::OnUserUpdate(float fElapsedTime)
 
 	timer += fElapsedTime;
 	//if (false){
-	if (timer > 0.5) {
+	if (timer > 1) {
 		timer = 0.0;
 		nRow = std::rand() % 6;
-		size_t nRowLen = nRow == 5 ? 7 : 10;
+		int nRowLen = nRow == 5 ? 7 : 10;
 		nCol = std::rand() % nRowLen;
 	}
 
-	olc::vf2d dimCard = { 94, 141 };
+	olc::vf2d screenCenter = dimCard / 2.0;
+
+	DrawPartialRotatedDecal(mouse, decDemo, timer * 3.14159, screenCenter, GetCardOffset(nRow, nCol), dimCard);
+	//DrawPartialDecal(mouse, decDemo, GetCardOffset(nRow, nCol), dimCard);
+
+	return true;
+}
+
+olc::vf2d Example::GetCardOffset(int nRow, int nCol)
+{
 	olc::vf2d offsetCard = { 10, 9 };
 
 	offsetCard.x += nCol * dimCard.x;
 	offsetCard.y += nRow * dimCard.y;
-
-	DrawPartialDecal(mouse, decDemo, offsetCard, dimCard);
-
-	return true;
+	return offsetCard;
 }
