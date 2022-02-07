407072c6703cd907a55a2036931af7ff074a4b95
DavidTret <davidtrettin@gmail.com>
2022-02-07 09:43:08 -0600

    Simplified app structure and added stb_image.h.

--- unoApp/UnoApp.cpp
+++ unoApp/UnoApp.cpp
@@ -1,18 +1,38 @@
-#include <iostream>
-#include "uno/GameLib.h"
-#include "uno/Example.h"
+#define OLC_PGE_APPLICATION
+#define OLC_IMAGE_STB
+#include "olcPixelGameEngine.h"
+
+// Override base class with your custom functionality
+class Example : public olc::PixelGameEngine
+{
+public:
+	Example()
+	{
+		// Name your application
+		sAppName = "Example";
+	}
+
+public:
+	bool OnUserCreate() override
+	{
+		// Called once at the start, so create things here
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		// Called once per frame, draws random coloured pixels
+		for (int x = 0; x < ScreenWidth(); x++)
+			for (int y = 0; y < ScreenHeight(); y++)
+				Draw(x, y, olc::Pixel(rand() % 256, rand() % 256, rand() % 256));
+		return true;
+	}
+};
 
 int main()
 {
-    //
-    // Seed random number generator.
-    std::srand((unsigned int)std::time(0));
-
-    //FeatureTest(); // Comment out if desired.
-    //KeyInputTest();
-    //gameLoop();
-
-    Example demo;
-    if (demo.Construct(256, 240, 4, 4))
-        demo.Start();
+	Example demo;
+	if (demo.Construct(256, 240, 4, 4))
+		demo.Start();
+	return 0;
 }
